3b7c05ae9ca01bc17c6d28015c685be9ba6c518d
johsteven <johsteven222004@gmail.com>
2021-07-16 20:38:27 +0700

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,36 @@
-const testimonialSwiper = new Swiper(".testimonial-swipe", {
-    slidesPerView: "auto",
-    pagination: {
-        el: ".testimonial-swipe-pagination",
-    }
-});
+// Variables
 
+// Functions
 
-const classesSwiper = new Swiper(".classes-swipe", {
-    slidesPerView: "auto",
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: "#swiper-button-next",
-        prevEl: "#swiper-button-prev",
-    },
-});
+// Main
+window.addEventListener("load", init);
+
+function init() {
+    load();
+    eventListener();
+}
+
+function load() {
+    const testimonialSwiper = new Swiper("#testimonial-swipe-container", {
+        slidesPerView: "auto",
+        pagination: {
+            el: "#testimonial-swipe-pagination",
+        }
+    });
+
+    const classesSwiper = new Swiper("#classes-swipe-container", {
+        slidesPerView: "auto",
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+            nextEl: "#swiper-button-next",
+            prevEl: "#swiper-button-prev",
+        },
+    });
+
+    AOS.init({
+        once: true,
+    });
+}
+
+function eventListener() {}
